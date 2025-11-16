5b624638fcae634758e3d510e9d7edb520107267
Ashwani Kumar <149175936+ashwanikumar107@users.noreply.github.com>
2025-11-16 16:32:08 +0530

    Update listing.js

--- routes/listing.js
+++ routes/listing.js
@@ -7,7 +7,7 @@ const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listings.js");
 const multer = require('multer');
-const { storage } = require("../cloudconfig.js");
+const { storage } = require("../cloudConfig.js");
 const upload = multer({ storage });
 
 router.route("/")
