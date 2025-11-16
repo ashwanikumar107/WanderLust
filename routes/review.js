4c8bb3b3da19021a7ae95df14ac1592e6501c6c5
Ashwani Kumar <149175936+ashwanikumar107@users.noreply.github.com>
2025-11-16 16:36:41 +0530

    Update review.js

--- routes/review.js
+++ routes/review.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router({ mergeParams: true });
 const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
-const Review = require("C:\\Users\\hp\\Desktop\\MajorProject\\models\\review.js");
-const Listing = require("C:\\Users\\hp\\Desktop\\MajorProject\\models\\listing.js");
+const Review = require("../models/review.js");
+const Listing = require("../models/listing.js");
 const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware.js");
 const reviewController = require("../controllers/reviews.js");
 
