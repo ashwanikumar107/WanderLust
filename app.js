12087fa31f7317bae4a7f3436ff65bc11e9ed28c
Ashwani Kumar <149175936+ashwanikumar107@users.noreply.github.com>
2026-02-18 12:47:29 +0530

    Clean up app.js and add trending listings route
    Removed commented-out code and added a new route for trending listings.

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const dbUrl = process.env.ATLASDB_URL;
 
 main()
@@ -66,10 +65,6 @@ const sessionOptions = {
     },
 };
 
-// app.get("/",(req,res) =>{
-//     res.send("Hi, I am root");
-// });
-
 app.use(session(sessionOptions));
 app.use(flash());
 
@@ -87,16 +82,9 @@ app.use((req, res, next) => {
     next();
 });
 
-// app.get("/demouser", async (req, res) => {
-//     let fakeuser = new User({
-//         email: "student@gmail.com",
-//         username: "delta-student"
-//     });
-
-//     let registeredUser = await User.register(fakeuser, "helloworld");
-//     res.send(registeredUser);
-// });
-
+app.get("/listings/trending", (req, res) => {
+    res.render("Category/trending");
+});
 
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
